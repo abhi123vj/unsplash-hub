304e1415a0cac04a79916b0e41c540682786dd1a
4bh16am <120167005+4bh1zd@users.noreply.github.com>
2024-07-27 20:27:50 +0530

    Initial commit

---
+++ app/src/main/java/app/web/abh1ram/unsplashhub/MainActivity.kt
@@ -0,0 +1,28 @@
+package app.web.abh1ram.unsplashhub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.web.abh1ram.unsplashhub.ui.theme.UnsplashHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnsplashHubTheme {
+
+            }
+        }
+    }
+}
+
+
